ba441e021e4cd0232c0ea24a1f70cdacd9684eef
Matei-Constantin <matei-constantin.clivet@student.tuiasi.ro>
2025-11-11 15:22:00 +0200

    Added training model for CNN.

---
+++ src/train_model.py
@@ -0,0 +1,53 @@
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+
+
+DATA_DIR = "../data/splits"
+IMG_SIZE = (128, 128) #dimenisunea la care dam rescale
+BATCH_SIZE = 4
+EPOCHS = 10
+CLASSES = ["acne", "eczema", "psoriasis", "melanoma"]
+
+#rescale la imagini
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+
+train_gen = train_datagen.flow_from_directory(
+    os.path.join(DATA_DIR, "train"),
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    color_mode="grayscale"
+)
+
+val_gen = val_datagen.flow_from_directory(
+    os.path.join(DATA_DIR, "val"),
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+    color_mode="grayscale"
+)
+
+model = Sequential([
+    Conv2D(32, (3,3), activation="relu", input_shape=(IMG_SIZE[0], IMG_SIZE[1], 1)), # 32 de filtre pentru trasaturi
+    MaxPooling2D(2,2), #pentru eficienta reducem imaginea la jumatate
+    Conv2D(64, (3,3), activation="relu"), # caracteristici complexe
+    MaxPooling2D(2,2),
+    Flatten(), #transforma matricea in vector
+    Dense(128, activation="relu"),
+    Dropout(0.3),
+    Dense(len(CLASSES), activation="softmax")
+])
+
+model.compile(optimizer=Adam(learning_rate=0.001),
+              loss="categorical_crossentropy",
+              metrics=["accuracy"])
+
+model.fit(train_gen, validation_data=val_gen, epochs=EPOCHS)
+
+model.save("../model.keras")
+print("Model salvat ca model.keras")
